610d36a8bf87349ba540c1311227771711bd3a10
Sunilmaske402 <144650809+Sunilmaske402@users.noreply.github.com>
2023-09-11 10:57:27 +0530

    Add files via upload
    Day 1

---
+++ AmarBank.java
@@ -0,0 +1,13 @@
+
+class AmarBank
+{
+	public static void main(String arg[])
+	{	
+		int interest;
+		int Amount=25000, principle = 11000, rate=2, time=1;
+		interest = principle*rate*time/100;
+		System.out.println("Amar has balence Amount Rs."+(Amount-principle) );
+		System.out.println("Interest on "+Amount+" for 12 moonth by 2% is "+interest );
+						
+	}
+}	
